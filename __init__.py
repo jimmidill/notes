900deac1606deff5b7824cd687e11130951e1ffe
James.Dillon <james.dillon@moneysupermarket.com>
2022-05-23 16:34:36 +0100

    Add log_in page

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 import os
-from flask import Flask, render_template, redirect, url_for, request, flash
+from flask import Flask, render_template, redirect, url_for, request, session, flash
 from flask_migrate import Migrate
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def create_app(test_config=None):
   app = Flask(__name__)
@@ -45,7 +45,31 @@ def create_app(test_config=None):
 
   @app.route('/log_in')
   def log_in():
-    return 'Login'
+    if request.method == 'POST':
+      username = request.form['username']
+      password = request.form['password']
+      error = None
+
+      user = User.query.filter_by(username=username).first()
+
+      if not user or not check_password_hash(user.password, password):
+        error = 'Username or password are incorrect.'
+
+      if error is None:
+        session.clear()
+        session['user_id'] = user.id 
+        return redirect(url_for('index'))
+
+      flash(error, 'error')
+    
+    return render_template('log_in.html')
+
+  @app.route('/')
+  def index():
+    return 'Index'
+
+
+
 
   return app
 
